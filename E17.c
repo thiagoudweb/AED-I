bc1b9c4e7849f9942df029a6c9ed302bb2488d8f
Thiago Silva <142932406+thiagoupsy@users.noreply.github.com>
2023-10-25 21:34:28 -0300

    Respostas

---
+++ E17.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+  
+  int port, mat, contador= 0;
+  float red;
+
+  while (1) {
+   
+    scanf("%d", &port);
+    scanf("%d", &mat);
+    scanf("%f", &red);
+
+    if (port < 0)
+    {
+        break;
+    }
+    else
+    {
+        if (port >= 40 && mat >= 21 && red >= 7)
+        {
+            contador++;
+        }
+        
+    }
+    
+  }
+
+  printf("%d\n", contador);
+
+  return 0;
+}
