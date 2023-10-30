c092543be9a1f1d74162c9ed6e8f46891e31b36e
Thiago Silva <142932406+thiagoupsy@users.noreply.github.com>
2023-10-30 00:43:07 -0300

    E16

--- E16.c
+++ E16.c
@@ -2,46 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct livro
+// struct para guardar os livros
+typedef struct prat_livros
 {
-    char nome[100];
+    char lib_livros[100];
+} array_livros;
 
-} livro;
+// remover o \0 no final da string na struct
+void remover_final(array_livros *remover_barran)
+{
+    int taman_len;
+    taman_len = strlen(remover_barran->lib_livros);
+    if (taman_len > 0 && remover_barran->lib_livros[taman_len - 1] == '\n')
+    {
+        remover_barran->lib_livros[taman_len - 1] = '\0';
+    }
+}
+
+// remover o \0 no final da string
+void remover_final_usuario(char *usuario_livro)
+{
+    int taman_len;
+    taman_len = strlen(usuario_livro);
+    if (taman_len > 0 && usuario_livro[taman_len - 1] == '\n')
+    {
+        usuario_livro[taman_len - 1] = '\0';
+    }
+}
 
 int main()
 {
-    int x;   
-    scanf("%d", &x);
-    livro *busca_livro = malloc(x*sizeof(livro));
+    int n, i, bool_comp;
+    scanf("%d", &n);
+    getchar();
+    char nome_usuario[100];
+    array_livros *guard_livros = malloc(n * sizeof(array_livros));
+    
+    for (i = 0; i < n; i++)
+    {
+        fgets(guard_livros[i].lib_livros, sizeof(guard_livros[i].lib_livros), stdin);
+        remover_final(guard_livros[i].lib_livros);
+    }
 
-    busca_livro 
+    fgets(nome_usuario, 100, stdin);
+    remover_final_usuario(nome_usuario);
+
+    bool_comp = 1;
+
+    for (i = 0; i < n; i++)
+    {
+        bool_comp = strcmp(nome_usuario, guard_livros[i].lib_livros);
+        if (bool_comp == 0)
+        {
+            bool_comp = 0;
+            break;
+        }
+    }
+    if (bool_comp == 0)
+    {
+        printf("Sim");
+    }
+    else{
+
+        printf("Nao");
+    }
 
 
 
 
 
 
-    return 0;
+
 }
-
-// faça um programa que receba n nomes de livros, depois receba mais um nome e diga se ele já foi citado anteriormente
-
-// Formato de entrada
-
-// um inteiro n seguido de n nomes de livro mais um nome de livro no final
-
-//      3
-
-//      O pequeno príncipe
-
-//      O Código Da Vince
-
-//      Calculo II
-
-//      O pequeno príncipe
-
-// Formato de saída
-
-// Sim ou Não se o livro estiver na lista
-
-//      Sim
