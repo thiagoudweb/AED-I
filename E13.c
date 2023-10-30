395edcef91892a1b9e678cd234bf20bd291cdae8
Thiago Silva <142932406+thiagoupsy@users.noreply.github.com>
2023-10-30 20:34:48 -0300

    +2

--- E13.c
+++ E13.c
@@ -5,56 +5,57 @@
 
 int main()
 {
-    int i, cont_par, cont_impar, y = 0;
+    int i, cont_par = 0, cont_impar = 0;
     int vetor_geral[15];
     int vetor_impar[5];
     int vetor_par[5];
+
     // lendo entrada do usuário
     for (i = 0; i < 15; i++)
     {
         scanf("%d", &vetor_geral[i]);
     }
 
-    // impares
-    cont_impar = 0;
+    // processando números
     for (i = 0; i < 15; i++)
     {
         if (vetor_geral[i] % 2 != 0)
         {
-            vetor_impar[y] = vetor_geral[i];
-            y++;
+            vetor_impar[cont_impar] = vetor_geral[i];
             cont_impar++;
             if (cont_impar == 5)
             {
-                for (y = 0; y < cont_impar; y++)
+                for (int y = 0; y < 5; y++)
                 {
                     printf("impar[%d] = %d\n", y, vetor_impar[y]);
                 }
-                y = 0;
                 cont_impar = 0;
+            }
+        }
+        else
+        {
+            vetor_par[cont_par] = vetor_geral[i];
+            cont_par++;
+            if (cont_par == 5)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    printf("par[%d] = %d\n", y, vetor_par[y]);
+                }
+                cont_par = 0;
             }
         }
     }
 
-    // pares
-    cont_par = 0;
-    for (i = 0; i < 15; i++)
+    // Imprimir os elementos restantes nos vetores impar e par
+    for (int y = 0; y < cont_impar; y++)
     {
-        if (vetor_geral[i] % 2 == 0)
-        {
-            vetor_par[y] = vetor_geral[i];
-            y++;
-            cont_par++;
-            if (cont_par == 5)
-            {
-                for (y = 0; y < cont_par; y++)
-                {
-                    printf("par[%d] = %d\n", y, vetor_par[y]);
-                }
-                y = 0;
-                cont_par = 0;
-            }
-        }
+        printf("impar[%d] = %d\n", y, vetor_impar[y]);
+    }
+
+    for (int y = 0; y < cont_par; y++)
+    {
+        printf("par[%d] = %d\n", y, vetor_par[y]);
     }
 
     return 0;
