c6b11b58e6dd149b24299d204b465a3e6fd6f937
Thiago Silva <thhzmonster@gmail.com>
2023-11-09 15:41:22 -0300

    ATV1

--- Sistema de matrícula.c
+++ Sistema de matrícula.c
@@ -1,95 +1,138 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-// banco de dados disciplinas
-typedef struct bd_disciplinas
-{
-    char codDisciplina[25];
-    char nomeDisciplina[100];
-
-} cad_disciplinas;
-
-// banco de dados alunos
-typedef struct bd_alunos
-{
-    char codigoDoAluno[50];
-    char nomeAlunoCompleto[500];
+struct Aluno {
+    char codigo[31];
+    char nome[501];
     int idade;
     int sexo;
-    int enem;
+    float nota_enem;
+};
 
-} cad_alunos;
+struct Disciplina {
+    char codigo[26];
+    char nome[101];
+    int num_alunos;
+    char **alunos_matriculados; 
+};
 
-// banco de dados matriculas
-typedef struct bd_matriculas
-{
-    cad_alunos aluno;
-    cad_disciplinas disciplina;
+int comparaNomes(const void *a, const void *b) {
+    return strcmp(((struct Aluno *)a)->nome, ((struct Aluno *)b)->nome);
+}
 
-} cad_matriculas;
+int main() {
+    int n, cont = 0;
+    scanf("%d", &n);
 
-int main()
-{
-    int numCad, numDis, numMat;
-    scanf("%d", &numCad);
-    getchar();
-    scanf("%d", &numDis);
-    getchar();
-    cad_alunos *vet_cad = malloc(numCad * sizeof(cad_alunos));
-    cad_disciplinas *vet_dis = malloc(numDis * sizeof(cad_disciplinas));
+    struct Aluno alunos[n];
+    int alunos_masculinos = 0;
+    int alunos_femininos = 0;
+    float soma_idades = 0.0;
+    float soma_idades_enem = 0.0;
 
-    // leitura dos dados dos alunos
-    for (int i = 0; i < numCad; i++) {
-        fgets(vet_cad[i].codigoDoAluno, 50, stdin);
-        fgets(vet_cad[i].nomeAlunoCompleto, 500, stdin);
-        scanf("%d", &vet_cad[i].idade);
-        scanf("%d", &vet_cad[i].sexo);
-        scanf("%d", &vet_cad[i].enem);
-        getchar();
+    for (int i = 0; i < n; i++) {
+        scanf("%s", alunos[i].codigo);
+        scanf(" %[^\n]", alunos[i].nome);
+        scanf("%d", &alunos[i].idade);
+        scanf("%d", &alunos[i].sexo);
+        scanf("%f", &alunos[i].nota_enem);
+
+        if (alunos[i].sexo == 0) {
+            alunos_femininos++;
+        } else {
+            alunos_masculinos++;
+        }
+
+        soma_idades += alunos[i].idade;
+        if (alunos[i].nota_enem > 7.5) {
+            soma_idades_enem += alunos[i].idade;
+            cont += 1;
+        }
     }
 
-    // leitura dos dados das disciplinas
-    for (int i = 0; i < numDis; i++) {
-        fgets(vet_dis[i].codDisciplina, 25, stdin);
-        fgets(vet_dis[i].nomeDisciplina, 100, stdin);
-        getchar();
+    qsort(alunos, n, sizeof(struct Aluno), comparaNomes);
+
+    int m;
+    scanf("%d", &m);
+
+    struct Disciplina disciplinas[m];
+
+    for (int i = 0; i < m; i++) {
+        scanf("%s", disciplinas[i].codigo);
+        scanf(" %[^\n]", disciplinas[i].nome);
+        disciplinas[i].num_alunos = 0;
+        disciplinas[i].alunos_matriculados = NULL;
     }
 
-    // leitura das matriculas
-    scanf("%d", &numMat);
-    getchar();
-    cad_matriculas *vet_mat = malloc(numMat * sizeof(cad_matriculas));
-    
-    for (int i = 0; i < numMat; i++) {
-        char codAluno[50], codDisciplina[25];
-        fgets(codAluno, 50, stdin);
-        fgets(codDisciplina, 25, stdin);
-        
-        // busca o aluno e a disciplina correspondentes
-        for (int j = 0; j < numCad; j++) {
-            if (strcmp(vet_cad[j].codigoDoAluno, codAluno) == 0) {
-                vet_mat[i].aluno = vet_cad[j];
+    int p;
+    scanf("%d", &p);
+
+    for (int i = 0; i < p; i++) {
+        char codigo_aluno[31];
+        char codigo_disciplina[26];
+        scanf("%s %s", codigo_aluno, codigo_disciplina);
+
+        struct Aluno *aluno = NULL;
+        struct Disciplina *disciplina = NULL;
+
+        for (int j = 0; j < n; j++) {
+            if (strcmp(alunos[j].codigo, codigo_aluno) == 0) {
+                aluno = &alunos[j];
                 break;
             }
         }
-        
-        for (int j = 0; j < numDis; j++) {
-            if (strcmp(vet_dis[j].codDisciplina, codDisciplina) == 0) {
-                vet_mat[i].disciplina = vet_dis[j];
+
+        for (int j = 0; j < m; j++) {
+            if (strcmp(disciplinas[j].codigo, codigo_disciplina) == 0) {
+                disciplina = &disciplinas[j];
                 break;
             }
         }
-        
-        getchar();
+
+        if (aluno != NULL && disciplina != NULL) {
+            disciplina->num_alunos++;
+            disciplina->alunos_matriculados = (char **)realloc(disciplina->alunos_matriculados, disciplina->num_alunos * sizeof(char *));
+            disciplina->alunos_matriculados[disciplina->num_alunos - 1] = aluno->nome;
+        }
     }
 
-    // TODO: implementar a lógica para gerar o relatório
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (strcmp(disciplinas[i].nome, disciplinas[j].nome) > 0) {
+                struct Disciplina temp = disciplinas[i];
+                disciplinas[i] = disciplinas[j];
+                disciplinas[j] = temp;
+            }
+        }
+    }
 
-    free(vet_cad);
-    free(vet_dis);
-    free(vet_mat);
+    printf("%d\n", alunos_masculinos);
+    printf("%d\n", alunos_femininos);
+    printf("%.2f\n", soma_idades / n);
+    if (soma_idades_enem > 0) {
+        printf("%.2f\n", soma_idades_enem / cont);
+    } else {
+        printf("0.00\n");
+    }
+
+    for (int i = 0; i < m; i++) {
+        printf("%s\n", disciplinas[i].nome);
+        printf("%d\n", disciplinas[i].num_alunos);
+        for (int j = 0; j < disciplinas[i].num_alunos; j++) {
+            for (int k = j + 1; k < disciplinas[i].num_alunos; k++) {
+                if (strcmp(disciplinas[i].alunos_matriculados[j], disciplinas[i].alunos_matriculados[k]) > 0) {
+                    char *temp = disciplinas[i].alunos_matriculados[j];
+                    disciplinas[i].alunos_matriculados[j] = disciplinas[i].alunos_matriculados[k];
+                    disciplinas[i].alunos_matriculados[k] = temp;
+                }
+            }
+        }
+        for (int j = 0; j < disciplinas[i].num_alunos; j++) {
+            printf("%s\n", disciplinas[i].alunos_matriculados[j]);
+        }
+    }
 
     return 0;
 }
-
---
+++ beecrowd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+
+
+
+
+    return 0;
+}
